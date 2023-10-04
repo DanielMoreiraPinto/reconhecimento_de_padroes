2d5c9d9d7f0ae21c3cb3168f9d7a668c543ae52b
joao.souza <joao.souza@nca.ufma.br>
2023-10-03 22:13:30 -0300

    add: Interface concluída

---
+++ interface/view.py
@@ -0,0 +1,46 @@
+from PyQt6.QtWidgets import QGraphicsView, QGraphicsScene
+from PyQt6.QtGui import QPainter
+from PyQt6.QtCore import Qt, pyqtSignal
+
+class View(QGraphicsView):
+    zoom_signal = pyqtSignal(float)
+    move_signal = pyqtSignal(int, int)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setRenderHint(QPainter.RenderHint.Antialiasing)
+        self.setRenderHint(QPainter.RenderHint.SmoothPixmapTransform)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+
+        self.dragging = False
+        self.last_pos = None
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.dragging = True
+            self.last_pos = event.pos()
+
+    def mouseMoveEvent(self, event):
+        if self.dragging:
+            delta = event.pos() - self.last_pos
+            self.last_pos = event.pos()
+            x = self.horizontalScrollBar().value() - delta.x()
+            y = self.verticalScrollBar().value() - delta.y()
+            self.move_signal.emit(x, y)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.dragging = False
+            self.last_pos = None
+
+    def wheelEvent(self, event):
+        self.setTransformationAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
+        zoom_in = 1.15
+        zoom_out = 1.0 / zoom_in
+        if event.pixelDelta().y() > 0:
+            self.zoom_signal.emit(zoom_in)
+        else:
+            self.zoom_signal.emit(zoom_out)
+
---
+++ setup_path.py
@@ -0,0 +1,9 @@
+import os
+
+path, _ = os.path.split(os.path.abspath(__file__))
+folder_relative_path = path.split(os.sep)[-1]
+absolute_root_path = path
+
+# descomenta aqui se quiser ver
+# print(folder_relative_path)
+# print(absolute_root_path)
