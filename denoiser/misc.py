f8cf49f137b193326c08c2a3ea290cd3cefb4a15
DanielMoreiraPinto <danielmoreirapinto.ti@gmail.com>
2023-10-07 17:41:51 -0300

    Patchifyer completo

--- denoiser/misc.py
+++ denoiser/misc.py
@@ -1,6 +1,24 @@
-from networks import simple_autoencoder, cbd_net, rid_net, dn_cnn
+# from networks import simple_autoencoder, cbd_net, rid_net, dn_cnn
 
-model = simple_autoencoder()
-model = cbd_net()
-model = rid_net()
-model = dn_cnn()
+# model = simple_autoencoder()
+# model = cbd_net()
+# model = rid_net()
+# model = dn_cnn()
+
+
+import patchify
+import cv2 as cv
+import os
+
+img_path = 'C:\\Users\\danie\\OneDrive\\Documentos\\Mestrado\\reconhecimento_de_padroes\\denoiser\\data\\RENOIR\\Mi3_Aligned\\Batch_001\\IMG_20160202_015216Reference.jpg'
+save_patches_path = 'C:\\Users\\danie\\OneDrive\\Documentos\\Mestrado\\reconhecimento_de_padroes\\denoiser\\data\\test\\patches'
+save_reconstructed_path = 'C:\\Users\\danie\\OneDrive\\Documentos\\Mestrado\\reconhecimento_de_padroes\\denoiser\\data\\test\\reconstructed.jpg'
+
+img = cv.imread(img_path)
+orig_shape = img.shape
+patches = patchify.split_image(img, patch_size=256)
+for i, patch in enumerate(patches):
+    cv.imwrite(os.path.join(save_patches_path, str(i)+'.jpg'), patch)
+    
+reconstructed = patchify.reconstruct_image(patches, orig_shape)
+cv.imwrite(save_reconstructed_path, reconstructed)
--- denoiser/patchify.py
+++ denoiser/patchify.py
@@ -1,34 +1,69 @@
+import math
+
 import numpy as np
+import cv2 as cv
 
 
 def split_image(image, patch_size=256):
     height, width, _ = image.shape
+
+    # Calculate the number of rows and columns needed for patches
+    num_rows = math.ceil(height / patch_size)
+    num_cols = math.ceil(width / patch_size)
+    
+    out_height = patch_size * num_rows
+    out_width = patch_size * num_cols
+
+    # Calculate the amount of padding needed
+    pad_height = out_height - height
+    pad_width = out_width - width
+
+    # Pad the image with zeros if necessary
+    if pad_height > 0 or pad_width > 0:
+        image = cv.copyMakeBorder(image, 0, pad_height, 0, pad_width, cv.BORDER_CONSTANT, value=0)
+
+    # Initialize an empty list to store the patches
     patches = []
 
-    for y in range(0, height, patch_size):
-        for x in range(0, width, patch_size):
+    # Iterate through the image and extract patches
+    for y in range(0, out_height, patch_size):
+        for x in range(0, out_height, patch_size):
             patch = image[y:y+patch_size, x:x+patch_size]
-            if patch.shape[:2] != (patch_size, patch_size):
-                # Pad the patch with zeros if it's smaller than the patch_size
-                pad_height = patch_size - patch.shape[0]
-                pad_width = patch_size - patch.shape[1]
-                patch = np.pad(patch, ((0, pad_height), (0, pad_width), (0, 0)), mode='constant')
             patches.append(patch)
 
-    return patches
+    return np.array(patches)
 
 def reconstruct_image(patches, image_shape):
-    height, width, _ = image_shape
-    patch_size = patches[0].shape[0]
-
-    reconstructed_image = np.zeros(image_shape, dtype=np.uint8)
-
-    patch_idx = 0
-    for y in range(0, height, patch_size):
-        for x in range(0, width, patch_size):
-            patch = patches[patch_idx]
-            reconstructed_image[y:y+patch_size, x:x+patch_size] = patch
-            patch_idx += 1
-
+    # Create an empty array for the reconstructed image
+    reconstructed_image = np.zeros(image_shape, dtype=patches.dtype)
+    patches_inside_img = image_shape[0] // patches.shape[1]
+    
+    # Initialize variables to keep track of current position
+    current_row, current_col = 0, 0
+    
+    # Iterate through the patches
+    for patch in patches:
+        # Calculate the maximum row and column indices for this patch
+        max_row = current_row + patch.shape[0]
+        max_col = current_col + patch.shape[1]
+        
+        # If the patch goes beyond the reconstructed image, 
+        # cut it off to fit
+        if max_row > image_shape[0]:
+            patch = patch[0:image_shape[0]-current_row, :, :]
+            max_row = image_shape[0]
+        if max_col > image_shape[1]:
+            patch = patch[:, 0:image_shape[1]-current_col, :]
+            max_col = image_shape[1]
+            
+        # Add the patch to the reconstructed image
+        reconstructed_image[current_row:max_row, current_col:max_col, :] = patch
+        
+        # Update the current row and col positions
+        current_col += patch.shape[1]
+        if current_col >= image_shape[1]:
+            current_row += patch.shape[0]
+            current_col = 0
+    
     return reconstructed_image
 
