605836654b77f455529f2e0d1ed646a9f32ea977
DanielMoreiraPinto <danielmoreirapinto.ti@gmail.com>
2023-12-13 22:43:42 -0300

    Detecção de blur implementada

--- interface/function_test.py
+++ interface/function_test.py
@@ -13,5 +13,11 @@ from deblur.test.testerClass import chamar_deblur, chamar_denoiser
 # cv2.imwrite('D:\\daniel_moreira\\reconhecimento_de_padroes\\reconhecimento_de_padroes\\ex_output\\GOPR0384_11_00-000001.png', img)
 # img = chamar_denoiser('D:\\daniel_moreira\\reconhecimento_de_padroes\\reconhecimento_de_padroes\\ex_input\\IMG_20160224_053715Noisy.jpg')
 # cv2.imwrite('D:\\daniel_moreira\\reconhecimento_de_padroes\\reconhecimento_de_padroes\\ex_output\\IMG_20160224_053715Noisy.jpg', img)
-img = chamar_deblur('D:\\daniel_moreira\\reconhecimento_de_padroes\\reconhecimento_de_padroes\\ex_input\\IMG_20160224_053715Noisy_denoised.jpg')
-cv2.imwrite('D:\\daniel_moreira\\reconhecimento_de_padroes\\reconhecimento_de_padroes\\ex_output\\IMG_20160224_053715Noisy_denoised.jpg', img)
+# img = chamar_deblur('D:\\daniel_moreira\\reconhecimento_de_padroes\\reconhecimento_de_padroes\\ex_input\\IMG_20160224_053715Noisy_denoised.jpg')
+# cv2.imwrite('D:\\daniel_moreira\\reconhecimento_de_padroes\\reconhecimento_de_padroes\\ex_output\\IMG_20160224_053715Noisy_denoised.jpg', img)
+
+from blurdetect.detect_blur import BlurDetector
+
+img = cv2.imread('D:\\daniel_moreira\\reconhecimento_de_padroes\\reconhecimento_de_padroes\\ex_input\\GOPR0384_11_00-000001.png')
+detector = BlurDetector()
+print(detector.detect_blur(img))
